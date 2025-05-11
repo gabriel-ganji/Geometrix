62b14f586a572ca30b8dbd8e68cc76725bb3c6b8
gabriel-ganji <gabrielganji01011010@gmail.com>
2025-05-10 21:13:21 -0300

    feat(header): styling

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,9 +1,19 @@
-import { Geometrix } from "./geometrix"
+import { Geometrix } from "./geometrix";
+import { LuSettings2 } from "react-icons/lu";
 
 export const Header = () => {
     return (
-        <header className="w-screen h-12 relative top-0 left-0">
-            <Geometrix />
+        <header className="flex items-start justify-start w-screen h-20 relative top-0 left-0 px-4 sm:px-12 py-4 border-b-2 border-b-main-text">
+            <div className="flex gap-2 items-center justify-start w-full h-full py-4">
+                <div className=" flex flex-wrap w-8 h-8">
+                    <div className="bg-main-text h-4 w-4"></div>
+                    <div className="bg-main-blue h-4 w-4"></div>
+                    <div className="bg-main-yellow h-4 w-4"></div>
+                    <div className="bg-main-orange h-4 w-4"></div>
+                </div>
+                <h2>GEOMETRIX</h2>
+            </div>
+            <LuSettings2 size={35} />
         </header>
     )
 }
